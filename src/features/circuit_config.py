016692d18028af2e26305727607e05a522d2c40f
Yashash-Mathur <yashash2212@gmail.com>
2026-08-31 23:13:57 +0530

    Fix features package directory

---
+++ src/features/circuit_config.py
@@ -0,0 +1,47 @@
+"""
+Circuit classifications used by the Universal F1 Prediction Framework.
+
+Configuration only.
+No data loading or feature calculations.
+"""
+
+STREET_CIRCUITS = {
+    "Australian Grand Prix",
+    "Azerbaijan Grand Prix",
+    "Canadian Grand Prix",
+    "Miami Grand Prix",
+    "Monaco Grand Prix",
+    "Saudi Arabian Grand Prix",
+    "Singapore Grand Prix",
+    "Las Vegas Grand Prix",
+}
+
+
+HIGH_SPEED_CIRCUITS = {
+    "Australian Grand Prix",
+    "Austrian Grand Prix",
+    "Azerbaijan Grand Prix",
+    "Belgian Grand Prix",
+    "British Grand Prix",
+    "Canadian Grand Prix",
+    "Italian Grand Prix",
+    "Las Vegas Grand Prix",
+    "Mexico City Grand Prix",
+    "Miami Grand Prix",
+    "Saudi Arabian Grand Prix",
+    "Styrian Grand Prix",
+    "70th Anniversary Grand Prix",
+    "Sakhir Grand Prix",
+}
+
+
+HIGH_DOWNFORCE_CIRCUITS = {
+    "Monaco Grand Prix",
+    "Singapore Grand Prix",
+    "Hungarian Grand Prix",
+    "Dutch Grand Prix",
+    "Japanese Grand Prix",
+    "Spanish Grand Prix",
+    "Qatar Grand Prix",
+    "British Grand Prix",
+}
